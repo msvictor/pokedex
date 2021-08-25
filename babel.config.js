bbda7d0752f9e2ab40fe449af86c24336e60574d
victorgabrielms <vgabriel.vg84@gmail.com>
2021-08-24 22:39:59 -0300

    setup path mappin with typescript and babel module resolver plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@types': './src/@types',
+          '@core': './src/core',
+          '@assets': './src/assets',
+          '@modules': './src/modules',
+          '@components': './src/modules/_shared/components',
+          '@details': './src/modules/details',
+          '@home': './src/modules/home',
+        },
+      },
+    ],
+  ],
 };
