5f35cc7d723515bdcb00b2093fa13635d57a5ebf
victorgabrielms <vgabriel.vg84@gmail.com>
2021-09-03 21:50:41 -0300

    alter create rows function to a usecallback

--- src/modules/home/components/PokeList/index.tsx
+++ src/modules/home/components/PokeList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { FlatList } from 'react-native';
 import {
   NavigationProp,
@@ -22,62 +22,74 @@ export const PokeList: React.FC<iPokeListProps> = ({
   const COLUMNS_NUMBER = 2;
   const navigation = useNavigation<NavigationProp<ParamListBase>>();
 
-  function createRows(data: POKEMON[], columns: number): POKEMON[] {
-    const rows = Math.floor(data.length / columns);
+  const [filteredPokemons, setFilteredPokemons] = useState(pokemons);
 
-    let lastRowElements = data.length - rows * columns;
+  const createRows = useCallback(
+    (data: POKEMON[], columns: number): POKEMON[] => {
+      const rows = Math.floor(data.length / columns);
 
-    while (lastRowElements !== columns && lastRowElements !== 0) {
-      data.push({
-        empty: true,
-        id: lastRowElements,
-        name: `empty-${lastRowElements}`,
-        num: `empty-${lastRowElements}`,
-        img: `empty-${lastRowElements}`,
-        type: [],
-        height: `empty-${lastRowElements}`,
-        weight: `empty-${lastRowElements}`,
-        candy: `empty-${lastRowElements}`,
-        candy_count: lastRowElements,
-        egg: `empty-${lastRowElements}`,
-        spawn_chance: lastRowElements,
-        avg_spawns: lastRowElements,
-        spawn_time: `empty-${lastRowElements}`,
-        multipliers: [],
-        weaknesses: [],
-      });
-      lastRowElements += 1;
-    }
+      let lastRowElements = data.length - rows * columns;
 
-    return data;
-  }
+      while (lastRowElements !== columns && lastRowElements !== 0) {
+        data.push({
+          empty: true,
+          id: lastRowElements,
+          name: `empty-${lastRowElements}`,
+          num: `empty-${lastRowElements}`,
+          img: `empty-${lastRowElements}`,
+          type: [],
+          height: `empty-${lastRowElements}`,
+          weight: `empty-${lastRowElements}`,
+          candy: `empty-${lastRowElements}`,
+          candy_count: lastRowElements,
+          egg: `empty-${lastRowElements}`,
+          spawn_chance: lastRowElements,
+          avg_spawns: lastRowElements,
+          spawn_time: `empty-${lastRowElements}`,
+          multipliers: [],
+          weaknesses: [],
+        });
+        lastRowElements += 1;
+      }
 
-  return (
-    <FlatList
-      data={createRows(pokemons, COLUMNS_NUMBER)}
-      numColumns={COLUMNS_NUMBER}
-      keyExtractor={(item) => item.num}
-      ListHeaderComponent={header}
-      columnWrapperStyle={{
-        paddingHorizontal: 12,
-      }}
-      renderItem={({ item }) => {
-        return (
-          <PokeCard
-            name={item.name}
-            num={item.num}
-            type={item.type}
-            imageUrl={item.img}
-            empty={item.empty || false}
-            onPress={() => {
-              if (item.empty) {
-                return;
-              }
-              navigation.navigate('Details', item);
-            }}
-          />
-        );
-      }}
-    />
+      return data;
+    },
+    []
+  );
+
+  const filterPokemonsByName = useCallback(() => {
+    //
+  }, []);
+
+  return useMemo(
+    () => (
+      <FlatList
+        data={createRows(filteredPokemons, COLUMNS_NUMBER)}
+        numColumns={COLUMNS_NUMBER}
+        keyExtractor={(item) => item.num}
+        ListHeaderComponent={header}
+        columnWrapperStyle={{
+          paddingHorizontal: 12,
+        }}
+        renderItem={({ item }) => {
+          return (
+            <PokeCard
+              name={item.name}
+              num={item.num}
+              type={item.type}
+              imageUrl={item.img}
+              empty={item.empty || false}
+              onPress={() => {
+                if (item.empty) {
+                  return;
+                }
+                navigation.navigate('Details', item);
+              }}
+            />
+          );
+        }}
+      />
+    ),
+    [header, createRows, filteredPokemons, navigation]
   );
 };
