cf836062a43bdfc186f5535b134a0071b9d79258
victorgabrielms <vgabriel.vg84@gmail.com>
2021-09-05 11:14:53 -0300

    move create an empty pokemon function to service

--- src/modules/home/pages/HomePage/index.tsx
+++ src/modules/home/pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
 import type { PokemonV1 } from '@modules/pokeapi/interfaces/PokemonV1';
 import type { NavigationProp, ParamListBase } from '@react-navigation/native';
 
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { usePokeapi } from '@modules/pokeapi/context';
+
+import SetEmptyPokemonService from '@modules/home/services/SetEmptyPokemonService';
 
 import { SafeAreaView, FlatList } from 'react-native';
 import { Header } from '@modules/home/components/Header';
@@ -12,75 +14,61 @@ import { PokeCard } from '@modules/home/components/PokeCard';
 export const HomePage: React.FC = () => {
   const COLUMNS_NUMBER = 2;
 
-  const { pokemons } = usePokeapi();
+  const { pokemons, filteredPokemons } = usePokeapi();
   const navigation = useNavigation<NavigationProp<ParamListBase>>();
 
   const createRows = useCallback(
-    (data: PokemonV1[], columns: number): PokemonV1[] => {
-      const rows = Math.floor(data.length / columns);
+    (pokeArray: PokemonV1[], columns: number): PokemonV1[] => {
+      const rows = Math.floor(pokeArray.length / columns);
 
-      let lastRowElements = data.length - rows * columns;
+      let lastRowElements = pokeArray.length - rows * columns;
 
       while (lastRowElements !== columns && lastRowElements !== 0) {
-        console.log(data.length);
-
-        data.push({
-          empty: true,
-          id: lastRowElements,
-          name: `empty-${lastRowElements}`,
-          num: `empty-${lastRowElements}`,
-          img: `empty-${lastRowElements}`,
-          type: [],
-          height: `empty-${lastRowElements}`,
-          weight: `empty-${lastRowElements}`,
-          candy: `empty-${lastRowElements}`,
-          candy_count: lastRowElements,
-          egg: `empty-${lastRowElements}`,
-          spawn_chance: lastRowElements,
-          avg_spawns: lastRowElements,
-          spawn_time: `empty-${lastRowElements}`,
-          multipliers: [],
-          weaknesses: [],
-        });
+        pokeArray.push(SetEmptyPokemonService.execute(lastRowElements));
         lastRowElements += 1;
       }
 
-      return data;
+      return pokeArray;
     },
     []
   );
 
-  const list = useMemo(
-    () => (
-      <FlatList
-        data={createRows(pokemons, COLUMNS_NUMBER)}
-        numColumns={COLUMNS_NUMBER}
-        keyExtractor={(item) => item.num}
-        ListHeaderComponent={Header}
-        columnWrapperStyle={{
-          paddingHorizontal: 12,
-        }}
-        renderItem={({ item }) => {
-          return (
-            <PokeCard
-              name={item.name}
-              num={item.num}
-              type={item.type}
-              imageUrl={item.img}
-              empty={item.empty || false}
-              onPress={() => {
-                if (item.empty) {
-                  return;
-                }
-                navigation.navigate('Details', item);
-              }}
-            />
-          );
-        }}
-      />
-    ),
-    [createRows, pokemons, navigation]
+  return (
+    <SafeAreaView>
+      {useMemo(
+        () => (
+          <FlatList
+            data={createRows(
+              filteredPokemons.length > 0 ? filteredPokemons : pokemons,
+              COLUMNS_NUMBER
+            )}
+            numColumns={COLUMNS_NUMBER}
+            keyExtractor={(item) => item.num}
+            ListHeaderComponent={Header}
+            columnWrapperStyle={{
+              paddingHorizontal: 12,
+            }}
+            renderItem={({ item }) => {
+              return (
+                <PokeCard
+                  name={item.name}
+                  num={item.num}
+                  type={item.type}
+                  imageUrl={item.img}
+                  empty={item.empty || false}
+                  onPress={() => {
+                    if (item.empty) {
+                      return;
+                    }
+                    navigation.navigate('Details', item);
+                  }}
+                />
+              );
+            }}
+          />
+        ),
+        [createRows, filteredPokemons, pokemons, navigation]
+      )}
+    </SafeAreaView>
   );
-
-  return <SafeAreaView>{list}</SafeAreaView>;
 };
---
+++ src/modules/home/services/SetEmptyPokemonService.ts
@@ -0,0 +1,26 @@
+import type { PokemonV1 } from '@modules/pokeapi/interfaces/PokemonV1';
+
+class SetEmptyPokemonService {
+  public execute(pokeId: number): PokemonV1 {
+    return {
+      empty: true,
+      id: pokeId,
+      name: '',
+      num: '',
+      img: '',
+      type: [],
+      height: '',
+      weight: '',
+      candy: '',
+      candy_count: 0,
+      egg: '',
+      spawn_chance: 0,
+      avg_spawns: 0,
+      spawn_time: '',
+      multipliers: [],
+      weaknesses: [],
+    };
+  }
+}
+
+export default new SetEmptyPokemonService();
